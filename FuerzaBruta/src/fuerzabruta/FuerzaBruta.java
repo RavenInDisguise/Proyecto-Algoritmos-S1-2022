7647321d6020c2006e0337577f6525027297deb3
Ignacio Brenes Mendez <brenesignacio95@estudiantec.cr>
2022-04-04 18:56:04 -0600

    cambios y ajustes en el codigo por favor revisar (creo que ahora si esta listo en serio) :(

--- FuerzaBruta/src/fuerzabruta/FuerzaBruta.java
+++ FuerzaBruta/src/fuerzabruta/FuerzaBruta.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Random;
 /**
  *
- * @author IBrenes
+ * @author Mónica Alfaro, Ignacio Brenes
  */
 public class FuerzaBruta {
     /**
@@ -17,9 +17,11 @@ public class FuerzaBruta {
      */
     public static ArrayList<List> allSubsets = new ArrayList<List>();
 
-    static void isSubsetSumZeroAleatorio(int arr[], int n){
+    static void isSubsetSumZero(int arr[], int n){
 
+        allSubsets=new ArrayList<List>();
         double startTime = System.currentTimeMillis(); // timer startup
+
 
         int a = 2;// "asignaciones counter"
         int c = 0;// "comparaciones counter"
@@ -55,7 +57,7 @@ public class FuerzaBruta {
             }
             c++;
 
-            // Print sum of picked elements.
+            // Save subsets
             c=c+2;
             if(sum==0 && !currentSubset.isEmpty()){
                 allSubsets.add(currentSubset);
@@ -66,104 +68,129 @@ public class FuerzaBruta {
 
         l=a+c;
 
-        System.out.println("asignaciones " + a);
-        System.out.println("comparaciones " + c);
-        System.out.println("lineas ejecutadas " + l);
+        //System.out.println(allSubsets.toString());
+        System.out.println("Asignaciones: " + a);
+        System.out.println("Comparaciones: " + c);
+        System.out.println("Lineas ejecutadas: " + l);
         double endTime = System.currentTimeMillis() - startTime; // The current time at the end of the program
-        System.out.println("tiempo de ejecucion "+endTime+" ms"); // The final print for the system timer
+        System.out.println("Tiempo de ejecución: "+endTime+" ms"+"\n"); // The final print for the system timer
     }
 
-    public static ArrayList<List> allSubsets2 = new ArrayList<List>();
-
-    static void isSubsetSumZeroQuemado(int arrQuemado[], int n2){
-
-        double startTime2 = System.currentTimeMillis(); // timer startup
-
-        int a2 = 2;// "asignaciones counter"
-        int c2 = 0;// "comparaciones counter"
-        int l2 = 0;// "lineas ejecutadas counter"
-
-        ArrayList<Integer> currentSubset2 = new ArrayList<>();
-        // There are total 2^n subsets
-        int total = 1 << n2;
-        a2++;
-
-        // Consider all numbers from 0 to 2^n - 1
-        for (int i = 0; i < total; i++) {
-
-            int sum = 0;
-
-            c2++;
-            a2++;
-
-            currentSubset2 = new ArrayList<>();
-
-            // Consider binary representation of
-            // current "i" to decide which elements
-            // to pick.
-
-            for (int j = 0; j < n2; j++){
-                c2 = c2+2;
-
-                if ((i & (1 << j)) != 0){
-                    currentSubset2.add(arrQuemado[j]);
-                    sum += arrQuemado[j];
-                    a2++;
-                }
-            }
-            c2++;
-
-            // Print sum of picked elements.
-            c2 = c2+2;
-            if(sum==0 && !currentSubset2.isEmpty()){
-                allSubsets2.add(currentSubset2);
-                //System.out.print(sum + " ");
-            }
-        }
-        c2++;
-
-        l2 = a2 + c2;
-
-        System.out.println("asignaciones " + a2);
-        System.out.println("comparaciones " + c2);
-        System.out.println("lineas ejecutadas " + l2);
-        double endTime2 = System.currentTimeMillis() - startTime2; // The current time at the end of the program
-        System.out.println("tiempo de ejecucion "+endTime2+" ms"); // The final print for the system timer
-    }
-
-
-
-
-
-
- /* Driver program to test above function */
+    /* Driver program to test above function */
     public static void main(String args[]){
 
-      //Randomdata array creation
+        int min = -15; //parametros para el minimo del numero aleatorio
+        int max = 15; //parametros para el maximo del numero aleatorio
 
-      Random rd = new Random(); // creating Random object
-      int[] arr = new int[5]; // input the lenght of the array
-      for (int i = 0; i < arr.length; i++) {
-         arr[i] = rd.nextInt(); // storing random integers in an array
-      }
-        int n = arr.length;
+        System.out.println("====================EJECUCION ALGORITMO FUERZA BRUTA================================\n\n");
 
+        for(int i=3; i <= 20;i++){
 
-        //Stored data array creation
+            //Randomdata array creation
+            Random rd = new Random(); // creating Random object
+            int[] arrAleatorio = new int[i]; // input the lenght of the array
 
-        int[] arrQuemado = new int[]{-1,1,-2,2-3};
-        int n2 = arrQuemado.length;
+            for (int j = 0; j < arrAleatorio.length; j++) {
+                arrAleatorio[j] = rd.nextInt(max-min+1) + min; // storing random integers in an array
 
-        //Random data
+            }
 
-        System.out.println("--------------------Datos aleatorios--------------------");
-        isSubsetSumZeroAleatorio(arr, n);
-        System.out.println("\nDesempeño del algoritmo con "+n+" elementos");
+            int n = arrAleatorio.length;
 
-        //Stored data
+            //Random data
+            System.out.println("--------------------Datos aleatorios--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n+" elementos");
+            isSubsetSumZero(arrAleatorio, n);
 
-        System.out.println("--------------------Datos quemados--------------------");
-        isSubsetSumZeroQuemado(arrQuemado, n2);
-        System.out.println("\nDesempeño del algoritmo con "+n+" elementos");
         }
+
+                    //Stored data array creation
+            int[] arrQuemado3 = new int[]{-1,1,-2};
+            int n3 = arrQuemado3.length;
+
+            int[] arrQuemado4 = new int[]{-1,1,-2,2};
+            int n4 = arrQuemado4.length;
+
+            int[] arrQuemado5 = new int[]{-1,1,-2,2,-3};
+            int n5 = arrQuemado5.length;
+
+            int[] arrQuemado6 = new int[]{-1,1,-2,2,-3,3};
+            int n6 = arrQuemado6.length;
+
+            int[] arrQuemado7 = new int[]{-1,1,-2,2,-3,3,-4};
+            int n7 = arrQuemado7.length;
+
+            int[] arrQuemado10 = new int[]{-1,1,-2,2,-3,3,-4,4,-5,5};
+            int n10 = arrQuemado10.length;
+
+            int[] arrQuemado11 = new int[]{-1,1,-2,2,-3,3,-4,4,-5,5,-6};
+            int n11 = arrQuemado11.length;
+
+            int[] arrQuemado12 = new int[]{-1,1,-2,2,-3,3,-4,4,-5,5,-6,6};
+            int n12 = arrQuemado12.length;
+
+            int[] arrQuemado13 = new int[]{-1,1,-2,2,-3,3,-4,4,-5,5,-6,6,-7};
+            int n13 = arrQuemado13.length;
+
+            int[] arrQuemado14 = new int[]{-1,1,-2,2,-3,3,-4,4,-5,5,-6,6,-7,7};
+            int n14 = arrQuemado14.length;
+
+            int[] arrQuemado15 = new int[]{-1,1,-2,2,-3,3,-4,4,-5,5,-6,6,-7,7,-8};
+            int n15 = arrQuemado15.length;
+
+            int[] arrQuemado20 = new int[]{-1,1,-2,2,-3,3,-4,4,-5,5,-6,6,-7,7,-8,8,-9,9,-10,10};
+            int n20 = arrQuemado20.length;
+
+            System.out.println("\n"+"\n*~~~~~~~~~~~~~~~~~~~~~~~~~~~INICIO DE SEGUNDA PARTE (DATOS QUEMADOS)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*"+"\n");
+
+            //Stored data
+            System.out.println("\n--------------------3 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n3+" elementos");
+            isSubsetSumZero(arrQuemado3, n3);
+
+            System.out.println("\n--------------------4 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n4+" elementos");
+            isSubsetSumZero(arrQuemado4, n4);
+
+            System.out.println("\n--------------------5 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n5+" elementos");
+            isSubsetSumZero(arrQuemado5, n5);
+
+            System.out.println("\n--------------------6 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n6+" elementos");
+            isSubsetSumZero(arrQuemado6, n6);
+
+            System.out.println("\n--------------------7 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n7+" elementos");
+            isSubsetSumZero(arrQuemado7, n7);
+
+            System.out.println("\n--------------------10 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n10+" elementos");
+            isSubsetSumZero(arrQuemado10, n10);
+
+            System.out.println("\n--------------------11 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n11+" elementos");
+            isSubsetSumZero(arrQuemado11, n11);
+
+            System.out.println("\n--------------------12 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n12+" elementos");
+            isSubsetSumZero(arrQuemado12, n12);
+
+            System.out.println("\n--------------------13 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n13+" elementos");
+            isSubsetSumZero(arrQuemado13, n13);
+
+            System.out.println("\n--------------------14 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n14+" elementos");
+            isSubsetSumZero(arrQuemado14, n14);
+
+            System.out.println("\n--------------------15 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n15+" elementos");
+            isSubsetSumZero(arrQuemado15, n15);
+
+            System.out.println("\n--------------------20 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo con "+n20+" elementos");
+            isSubsetSumZero(arrQuemado20, n20);
+
     }
+}
