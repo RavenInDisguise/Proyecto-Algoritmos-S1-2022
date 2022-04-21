e7210f89f13ea1cc2d7d8ee66c903272b0655496
Ignacio Brenes Mendez <brenesignacio95@estudiantec.cr>
2022-04-20 21:53:20 -0600

    correcion 20 de abril

--- FuerzaBruta/src/subsetzero/SubsetZero.java
+++ FuerzaBruta/src/subsetzero/SubsetZero.java
@@ -69,7 +69,7 @@ public class SubsetZero {
 
             c=c+2;
             // Save subsets
-            if(sum==0){
+            if(sum==0 && !currentSubset.isEmpty()){
                 allSubsets.add(currentSubset);
                 //System.out.print(sum + " ");
             }
@@ -101,6 +101,9 @@ public class SubsetZero {
        boolean sameValues=false;
        boolean sameValuesAdded=false;
 
+
+       allSubsets2.clear();
+
        for (int x=0; x < allSubsets.size(); x++) {
             int sum=0;
             for (int y=0; y < allSubsets.get(x).size(); y++) {
@@ -123,8 +126,13 @@ public class SubsetZero {
 
 
         }
+
+       allSubsets.clear();
+
         System.out.println(allSubsets2.toString());
     }
+
+    //__________________________________________________________________________________________________________________
 
     static int a2 = 5;// "asignaciones counter"
     static int c2 = 0;// "comparaciones counter"
@@ -134,14 +142,13 @@ public class SubsetZero {
     // to it to make it positive
     static int isSubsetSumZeroDP(int i, int s, ArrayList<Integer> currentSubset, int arr[], int n){
 
-
-
         // Base cases
         c2++;
         if (i == n){
             c2++;
             if (s == 0){
                 allSubsets.add(currentSubset);
+                a2++;
                 return 1;
             }else{
                 return 0;
@@ -149,6 +156,7 @@ public class SubsetZero {
         }
 
         allSubsets.add(currentSubset);
+        a2++;
 
         // Returns the value if a state is already solved
         c2++;
@@ -166,6 +174,7 @@ public class SubsetZero {
         currentSubset2=(ArrayList<Integer>) currentSubset.clone();
         a2++;
         currentSubset.add(arr[i]);
+        a2++;
         dp[i][s + arrSize] = isSubsetSumZeroDP(i + 1, s + arr[i], currentSubset, arr, n)
                 + isSubsetSumZeroDP(i + 1, s, currentSubset2, arr, n);
         a2++;
@@ -180,6 +189,15 @@ public class SubsetZero {
         //return dp[i][s + arrSize];
     }
 
+    static void printAC(int a, int c, double startTime2){
+
+        System.out.println("Asignaciones: " + a);
+        System.out.println("Comparaciones: " + c);
+        System.out.println("Lineas ejecutadas: " + (a+c));
+        double endTime2 = System.currentTimeMillis() - startTime2; // The current time at the end of the program
+        System.out.println("Tiempo de ejecución: "+endTime2+" ms"); // The final print for the system timer
+    }
+
 //__________________________________________________________________________________________________________________
 
 
@@ -189,7 +207,7 @@ public class SubsetZero {
         int min = -15; //parametros para el minimo del numero aleatorio
         int max = 15; //parametros para el maximo del numero aleatorio
 
-        for(int i=3; i <= 20;i++){
+        for(int i=3; i <= 21;i++){
 
             //Randomdata array creation
             Random rd = new Random(); // creating Random object
@@ -197,31 +215,24 @@ public class SubsetZero {
 
             for (int j = 0; j < arrAleatorio.length; j++) {
                 arrAleatorio[j] = rd.nextInt(max-min+1) + min; // storing random integers in an array
-
             }
 
-            System.out.println("====================EJECUCION ALGORITMO PROGRAMACIÓN DINÁMICA================================\n\n");
-
             int n = arrAleatorio.length;
-            ArrayList<Integer> currentSubset =  new ArrayList<>();
-            int arr[] = {-5, 1, 2, 3, 4, 5, 30};
-            int arrSize = arr.length;
-            double startTime2 = System.currentTimeMillis(); // timer startup
-            isSubsetSumZeroDP(0,0,currentSubset,arr,arrSize);
-            System.out.println("Asignaciones: " + a2);
-            System.out.println("Comparaciones: " + c2);
-            System.out.println("Lineas ejecutadas: " + (a2+c2));
-            double endTime2 = System.currentTimeMillis() - startTime2; // The current time at the end of the program
-            System.out.println("Tiempo de ejecución: "+endTime2+" ms"); // The final print for the system timer
-            printSubsets();
 
-            System.out.println("\n====================EJECUCION ALGORITMO FUERZA BRUTA================================\n\n");
+            System.out.println("====================EJECUCION ALGORITMO PROGRAMACIÓN DINÁMICA================================\n");
+
+            ArrayList<Integer> currentSubset =  new ArrayList<>();
+            double startTime2 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset,arrAleatorio, n);
+            printSubsets();
+            printAC(a2, c2, startTime2);
+
+            System.out.println("====================EJECUCION ALGORITMO FUERZA BRUTA================================\n");
 
             //Random data
             System.out.println("--------------------Datos aleatorios--------------------");
             System.out.println("\nDesempeño del algoritmo con "+n+" elementos");
             isSubsetSumZero(arrAleatorio, n);
-
         }
 
             //Stored data array creation
@@ -261,55 +272,152 @@ public class SubsetZero {
             int[] arrQuemado20 = new int[]{-1,1,-2,2,-3,3,-4,4,-5,5,-6,6,-7,7,-8,8,-9,9,-10,10};
             int n20 = arrQuemado20.length;
 
-            System.out.println("\n"+"\n*~~~~~~~~~~~~~~~~~~~~~~~~~~~INICIO DE SEGUNDA PARTE (DATOS QUEMADOS)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*"+"\n");
+            System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~INICIO DE SEGUNDA PARTE (DATOS QUEMADOS)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*"+"\n");
 
             //Stored data
             System.out.println("\n--------------------3 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n3+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n3+" elementos");
             isSubsetSumZero(arrQuemado3, n3);
 
+            System.out.println("\n--------------------3 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n3+" elementos");
+            ArrayList<Integer> currentSubset3 =  new ArrayList<>();
+            double startTime3 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset3,arrQuemado3, n3);
+            printSubsets();
+            printAC(a2, c2, startTime3);
+
             System.out.println("\n--------------------4 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n4+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n4+" elementos");
             isSubsetSumZero(arrQuemado4, n4);
 
+            System.out.println("\n--------------------4 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n4+" elementos");
+            ArrayList<Integer> currentSubset4 =  new ArrayList<>();
+            double startTime4 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset4,arrQuemado4, n4);
+            printSubsets();
+            printAC(a2, c2, startTime4);
+
             System.out.println("\n--------------------5 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n5+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n5+" elementos");
             isSubsetSumZero(arrQuemado5, n5);
 
+            System.out.println("\n--------------------5 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n5+" elementos");
+            ArrayList<Integer> currentSubset5 =  new ArrayList<>();
+            double startTime5 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset5,arrQuemado5, n5);
+            printSubsets();
+            printAC(a2, c2, startTime5);
+
             System.out.println("\n--------------------6 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n6+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n6+" elementos");
             isSubsetSumZero(arrQuemado6, n6);
 
+            System.out.println("\n--------------------6 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n6+" elementos");
+            ArrayList<Integer> currentSubset6 =  new ArrayList<>();
+            double startTime6 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset6,arrQuemado6, n6);
+            printSubsets();
+            printAC(a2, c2, startTime6);
+
             System.out.println("\n--------------------7 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n7+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n7+" elementos");
             isSubsetSumZero(arrQuemado7, n7);
 
+            System.out.println("\n--------------------7 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n7+" elementos");
+            ArrayList<Integer> currentSubset7 =  new ArrayList<>();
+            double startTime7 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset7,arrQuemado7, n7);
+            printSubsets();
+            printAC(a2, c2, startTime7);
+
             System.out.println("\n--------------------10 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n10+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n10+" elementos");
             isSubsetSumZero(arrQuemado10, n10);
 
+            System.out.println("\n--------------------10 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n10+" elementos");
+            ArrayList<Integer> currentSubset10 =  new ArrayList<>();
+            double startTime10 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset10,arrQuemado10, n10);
+            printSubsets();
+            printAC(a2, c2, startTime10);
+
             System.out.println("\n--------------------11 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n11+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n11+" elementos");
             isSubsetSumZero(arrQuemado11, n11);
 
+            System.out.println("\n--------------------11 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n11+" elementos");
+            ArrayList<Integer> currentSubset11 =  new ArrayList<>();
+            double startTime11 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset11,arrQuemado11, n11);
+            printSubsets();
+            printAC(a2, c2, startTime11);
+
             System.out.println("\n--------------------12 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n12+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n12+" elementos");
             isSubsetSumZero(arrQuemado12, n12);
 
+            System.out.println("\n--------------------12 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n12+" elementos");
+            ArrayList<Integer> currentSubset12 =  new ArrayList<>();
+            double startTime12 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset12,arrQuemado12, n12);
+            printSubsets();
+            printAC(a2, c2, startTime12);
+
             System.out.println("\n--------------------13 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n13+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n13+" elementos");
             isSubsetSumZero(arrQuemado13, n13);
 
+            System.out.println("\n--------------------13 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n13+" elementos");
+            ArrayList<Integer> currentSubset13 =  new ArrayList<>();
+            double startTime13 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset13,arrQuemado13, n13);
+            printSubsets();
+            printAC(a2, c2, startTime13);
+
             System.out.println("\n--------------------14 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n14+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n14+" elementos");
             isSubsetSumZero(arrQuemado14, n14);
 
+            System.out.println("\n--------------------14 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n14+" elementos");
+            ArrayList<Integer> currentSubset14 =  new ArrayList<>();
+            double startTime14 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset14,arrQuemado14, n14);
+            printSubsets();
+            printAC(a2, c2, startTime14);
+
             System.out.println("\n--------------------15 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n15+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n15+" elementos");
             isSubsetSumZero(arrQuemado15, n15);
 
+            System.out.println("\n--------------------15 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n15+" elementos");
+            ArrayList<Integer> currentSubset15 =  new ArrayList<>();
+            double startTime15 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset15,arrQuemado15, n15);
+            printSubsets();
+            printAC(a2, c2, startTime15);
+
             System.out.println("\n--------------------20 Datos quemados--------------------");
-            System.out.println("\nDesempeño del algoritmo con "+n20+" elementos");
+            System.out.println("\nDesempeño del algoritmo Fuerza Bruta con "+n20+" elementos");
             isSubsetSumZero(arrQuemado20, n20);
+
+            System.out.println("\n--------------------20 Datos quemados--------------------");
+            System.out.println("\nDesempeño del algoritmo Programacion Dinamica con "+n20+" elementos");
+            ArrayList<Integer> currentSubset20 =  new ArrayList<>();
+            double startTime20 = System.currentTimeMillis(); // timer startup
+            isSubsetSumZeroDP(0,0,currentSubset20,arrQuemado20, n20);
+            printSubsets();
+            printAC(a2, c2, startTime20);
+
     }
 }
